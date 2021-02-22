cc5afb7cd84a2b6f63550efd4615bc6bfab0a77d
shohei morikawa <sh.oh54t@gmail.com>
2021-02-22 23:33:04 +0900

    publish world frame

--- scripts/pdr.py
+++ scripts/pdr.py
@@ -40,25 +40,38 @@ class Node:
 		print(math.degrees(rpy[0]))
 		print('pitch')
 		print(math.degrees(rpy[1]))
-		mat = self.getRotationMatrix(rpy[0],rpy[1],rpy[2])
+		mat = getRotationMatrix(rpy[0],rpy[1],rpy[2])
 		accel_world = np.dot(mat,accel_calib.T)
 		print('accel world')
 		print(accel_world)
 
-	def getRotationMatrix(self,roll,pitch,yaw):
-		mat = np.zeros((3,3))
-		rot_x = np.array([[1,0,0],[0,math.cos(roll),-math.sin(roll)],[0,math.sin(roll),math.cos(roll)]])
-		rot_y = np.array([[math.cos(pitch),0,math.sin(pitch)],[0,1,0],[-math.sin(pitch),0,math.cos(pitch)]])
-		rot_z = np.array([[math.cos(yaw),-math.sin(yaw),0],[math.sin(yaw),math.cos(yaw),0],[0,0,1]])
-		return np.dot(rot_z,np.dot(rot_y,rot_x))
+		imu_world_out = Imu()
+		imu_world_out.header = data.header
+		imu_world_out.linear_acceleration.x = accel_world[0]
+		imu_world_out.linear_acceleration.y = accel_world[1]
+		imu_world_out.linear_acceleration.z = accel_world[2]
+
+		self.pub.publish(imu_world_out)
+		
+
 
 	def __init__(self):
 		rospy.init_node('pdr',anonymous=True)
 		rospy.Subscriber("/android/tango1/imu",Imu,self.ImuCallback)
+		self.pub = rospy.Publisher('/accel_world',Imu,queue_size=10)
+		
 		#rospy.Subscriber("/android/tango1/magnetic_field",MagneticField,self.MagneticFieldCallback)
+		self.rate = rospy.Rate(10)
 	def run(self):
 		print('node is running')
-		rospy.spin()
+		while not rospy.is_shutdown():
+			self.rate.sleep()
+def getRotationMatrix(roll,pitch,yaw):
+	mat = np.zeros((3,3))
+	rot_x = np.array([[1,0,0],[0,math.cos(roll),-math.sin(roll)],[0,math.sin(roll),math.cos(roll)]])
+	rot_y = np.array([[math.cos(pitch),0,math.sin(pitch)],[0,1,0],[-math.sin(pitch),0,math.cos(pitch)]])
+	rot_z = np.array([[math.cos(yaw),-math.sin(yaw),0],[math.sin(yaw),math.cos(yaw),0],[0,0,1]])
+	return np.dot(rot_z,np.dot(rot_y,rot_x))
 def main():
 	node = Node()
 	node.run()
